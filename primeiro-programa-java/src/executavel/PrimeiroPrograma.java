72e653cb2397df0826fab5f76db7a5e3e382af07
JRSALVES <jeruso.nascimento@outlook.com>
2024-11-20 18:33:10 -0300

    Initial commit

---
+++ primeiro-programa-java/src/executavel/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package executavel;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Meu Primeiro Programa em Java!");
+		System.out.println("Gosteiiiiii!");
+	}
+
+}
