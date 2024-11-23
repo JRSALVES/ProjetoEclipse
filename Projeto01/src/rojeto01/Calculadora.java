05bca6b061ed3dcd887fb0eac261a9a4d6f56485
JRSALVES <jeruso.nascimento@outlook.com>
2024-11-23 16:15:27 -0300

    Criação do projeto 01

---
+++ Projeto01/src/rojeto01/Calculadora.java
@@ -0,0 +1,17 @@
+package rojeto01;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		 int numero01 = 5;
+		 
+		 int numero02 = 10;
+		 
+		 int soma = numero01 + numero02;
+		 
+		 
+		 System.out.println(soma);
+
+	}
+
+}
