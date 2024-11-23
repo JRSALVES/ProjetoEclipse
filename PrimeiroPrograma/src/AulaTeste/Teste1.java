020f556138a480f36f41db8542faed0b856ac55e
JRSALVES <jeruso.nascimento@outlook.com>
2024-11-23 16:06:12 -0300

    Atribuido dados as classes

--- PrimeiroPrograma/src/AulaTeste/Teste1.java
+++ PrimeiroPrograma/src/AulaTeste/Teste1.java
@@ -2,8 +2,20 @@ package AulaTeste;
 
 public class Teste1 {
 
-	public static void main(String[] args) {
-		System.out.println("Meu primeiro programa em Java");
-	}
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro programa em Java\n");
 
+        // Create a Pessoa object
+        Pessoa jeruso = new Pessoa();
+
+        // Set attributes
+        jeruso.setName("Jeruso Alves Nascimento");
+        jeruso.setAge(57);
+        jeruso.setEmail("jerusoalves@gmail.com");
+
+        // Output the attributes
+        System.out.println("Nome: " + jeruso.getName());
+        System.out.println("Idade: " + jeruso.getAge());
+        System.out.println("Email: " + jeruso.getEmail());
+    }
 }
